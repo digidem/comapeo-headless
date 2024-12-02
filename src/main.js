2dd65bbf54a9949cf276cfdc82331055b0f09d47
Evan Hahn <me@evanhahn.com>
2024-12-02 15:12:01 -0600

    feat: connecting local peers

--- src/main.js
+++ src/main.js
@@ -4,36 +4,13 @@ import process from 'node:process'
 import * as path from 'node:path'
 import { temporaryDirectory } from 'tempy'
 import * as crypto from 'node:crypto'
+import { isIPv4 } from 'node:net'
 import Fastify from 'fastify'
+/** @import { Service } from 'bonjour-service' */
 
 // TODO: organize imports
 
 const BONJOUR_SERVICE_TYPE = 'comapeo'
-
-/**
- * @returns {(fn: () => unknown) => void}
- */
-function gracefulCloser() {
-  /** @type {Array<() => unknown>} */ const fns = []
-
-  /**
-   * @param {NodeJS.Signals} signal
-   * @returns {Promise<void>}
-   */
-  const onSignal = async (signal) => {
-    process.off('SIGINT', onSignal)
-    process.off('SIGTERM', onSignal)
-
-    await Promise.allSettled(fns.map((fn) => fn()))
-
-    process.kill(process.pid, signal)
-  }
-
-  process.once('SIGINT', onSignal)
-  process.once('SIGTERM', onSignal)
-
-  return fns.push.bind(fns)
-}
 
 async function main() {
   const beforeExit = gracefulCloser()
@@ -53,8 +30,13 @@ async function main() {
   })
   console.log(`Created manager with device ID ${mapeoManager.deviceId}.`)
 
+  // TODO: Set device info
+
   const { name, port } = await mapeoManager.startLocalPeerDiscoveryServer()
-  beforeExit(() => mapeoManager.stopLocalPeerDiscoveryServer())
+  beforeExit(() => {
+    console.log('Stopping local peer discovery server...')
+    return mapeoManager.stopLocalPeerDiscoveryServer()
+  })
   console.log(
     `Started local peer discovery server on port ${port} with name ${name}.`,
   )
@@ -66,16 +48,64 @@ async function main() {
     name,
     port,
   })
-  beforeExit(() => publishedService.stop?.())
+  beforeExit(() => {
+    console.log('Stopping Bonjour service...')
+    return publishedService.stop?.()
+  })
   console.log(`Published Bonjour service with name ${name}.`)
 
   const browser = bonjour.find({ type: BONJOUR_SERVICE_TYPE }, (service) => {
     if (service.name === name) return
-    // TODO: Do something with this
-    console.log('Found a peer:', service)
+    console.log(
+      `Found peer named ${JSON.stringify(service.name)} on port ${service.port} with address(es) ${JSON.stringify(service.addresses)}.`,
+    )
+    const peer = bonjourServiceToMapeoPeer(service)
+    if (peer) {
+      mapeoManager.connectLocalPeer(peer)
+      console.log(`Connected peer ${JSON.stringify(service.name)}.`)
+    } else {
+      console.log(
+        `Peer ${JSON.stringify(service.name)} could not be converted to a Mapeo peer; ignoring.`,
+      )
+    }
   })
-  beforeExit(() => browser.stop())
+  beforeExit(() => {
+    console.log('Stopping Bonjour browser...')
+    return browser.stop()
+  })
   console.log('Started Bonjour browser.')
+}
+
+/**
+ * @returns {(fn: () => unknown) => void}
+ */
+function gracefulCloser() {
+  /** @type {Array<() => unknown>} */ const fns = []
+
+  const onSignal = async () => {
+    process.off('SIGINT', onSignal)
+    process.off('SIGTERM', onSignal)
+
+    await Promise.allSettled(fns.map((fn) => fn()))
+
+    process.exit(0)
+  }
+
+  process.once('SIGINT', onSignal)
+  process.once('SIGTERM', onSignal)
+
+  return fns.push.bind(fns)
+}
+
+/**
+ * @param {Readonly<Service>} service
+ * @returns {null | Parameters<typeof MapeoManager.prototype.connectLocalPeer>[0]}
+ */
+function bonjourServiceToMapeoPeer({ name, port, addresses = [] }) {
+  // Prefer IPv4 addresses.
+  const address = addresses.find(isIPv4) || addresses[0]
+  if (!address) return null
+  return { name, port, address }
 }
 
 main().catch((err) => {
